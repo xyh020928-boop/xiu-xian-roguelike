e25d52c63711156127b39ac1eb3db60fcad68449
xyh020928-boop <xyh020928@gmail.com>
2026-05-03 13:55:48 +1000

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/xiu-xian-roguelike/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-});
+    assetsDir: 'assets'
+  }
+})
